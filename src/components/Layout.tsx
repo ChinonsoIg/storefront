241d16c592b49380b1fd9e5795ac1bcb4bada3a8
Ojoechem Chinonso <ojononso@yahoo.com>
2023-10-10 09:36:11 +0100

    fix navbar issue

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,7 +11,7 @@ const [searchTerm, setSearchTerm] = useState("");
 
 
   return (
-    <div className="flex flex-col items-center justify-start h-screen w-full">
+    <div className="flex flex-col items-center justify-start h-screen w-full max-w-[1240px]">
       <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <div className="w-full max-w-[1240px] px-5 pt-0">
         {children}
@@ -22,3 +22,17 @@ const [searchTerm, setSearchTerm] = useState("");
 }
 
 export default Layout;
+
+export const getServerSideProps = async () => {
+  // Fetch data from external API
+  const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+  const resProducts = await fetch(`${BASE_URL}/products/storefront`);
+
+  const resCategories = await fetch(`${BASE_URL}/categories`);
+
+  const products = await resProducts.json();
+  const categories = await resCategories.json();
+
+  return { props: { products, categories } };
+}
--- src/pages/ornaments/index.tsx
+++ src/pages/ornaments/index.tsx
@@ -1,86 +1,27 @@
-import styles from "@/styles/globals.module.css";
-import Head from "next/head";
-import Image, { ImageLoaderProps } from "next/image";
-import { Inter } from "@next/font/google"
-import { useEffect, useState } from "react";
-import Link from "next/link";
-import "animate.css";
-
-import Product from "../components/Product";
-import Layout from "../components/Layout";
+import React from "react";
+import Image from "next/image";
+import Layout from "@/components/Layout";
 
 import advert_trainer from "/public/images/advert_trainer.png";
 import advert_top_purple from "/public/images/advert_top_purple.png";
 import advert_apple_watch from "/public/images/advert_apple_watch.png";
 
-interface ICategory {
-  categoryName: string;
-  createdAt: string;
-  createdBy: string;
-  updatedAt: string;
-  _id: string;
-}
-
-interface IProducts {
-  categoryId: string;
-  createdAt: string;
-  createdBy: string;
-  description: string;
-  image: Array<string>;
-  name: string;
-  price: number;
-  quantity: number;
-  status: string;
-  updatedAt: string;
-  _id: string;
-}
-
-interface IProps {
-  products: {
-    products: Array<IProducts>;
-    productsPerPage: number;
-    totalProducts: number;
-  };
-  categories: {
-    categories: Array<ICategory>;
-    count: number;
-  }
-}
-
-
-const myLoader = ({ src, width, quality }: ImageLoaderProps) => {
-  return `${src}?w=${width}&q=${quality || 75}`
-}
-
-const Home = ({ products, categories }: IProps) => {
-  const [phrase, setPhrase] = useState("");
-
-  const findCategory = (id: string) => {
-    const found = categories.categories.find((category) => category._id == id)?.categoryName;
-    return found;
-  };
-
-  const categoriesNames = [...products.products.map(product => findCategory(product.categoryId))];
-
-  // console.log("gh: ", categoriesNames);
-  console.log("ghh: ", products);
-
-  // if (phrase) {
-  //   products = products.filter(p => p.name.toLowerCase().includes(phrase));
-  // }
-
+const Ornaments = () => {
   return (
     <Layout>
       <div className="relative flex items-start gap-2">
         <nav className="hidden lg:block fixed top-24 w-48 h-96 px-4 py-2 shadow-md rounded-md border bg-white">
           <ul className="py-2">
-            {categories.categories.map((category) => (
+            {/* {categories.categories.map((category) => (
               <li key={category._id} className="capitalize py-1.5">
-                <Link href={{ pathname: `/categories`, query: { categoryId: category._id } }}>
+                <Link href={{ 
+                  pathname: `/${category.categoryName}`, 
+                  // query: { categoryId: category._id } 
+                }}>
                   {category.categoryName}
                 </Link>
               </li>
-            ))}
+            ))} */}
           </ul>
         </nav>
         <main className="ml-0 lg:ml-56 mt-4">
@@ -92,38 +33,38 @@ const Home = ({ products, categories }: IProps) => {
             </p>
 
             <div className="absolute bottom-[3%] left-[5%] z-10">
-              <Image
+              {/* <Image
                 loader={myLoader}
                 src={advert_trainer}
                 height={100}
                 width={100}
                 alt="l"
-              />
+              /> */}
             </div>
             <div className="absolute bottom-[25%] right-[4%] z-10">
-              <Image
+              {/* <Image
                 loader={myLoader}
                 src={advert_apple_watch}
                 height={100}
                 width={100}
                 alt="l"
-              />
+              /> */}
             </div>
             <div className="absolute top-[3%] left-[5%] z-10">
-              <Image
+              {/* <Image
                 loader={myLoader}
                 src={advert_top_purple}
                 height={100}
                 width={100}
                 alt="l"
-              />
+              /> */}
             </div>
             
             <div className="absolute z-0 h-96 w-full advert-table bg-purple-700 rounded-md">
             </div>
           </section>
-          <section className="my-8">
-            {categories.categories.map(category => (
+          <section className="my-8 w-full"> section
+            {/* {categories.categories.map(category => (
               <div key={category._id} className="">
                 {products.products.find(p => p.categoryId === category._id) && (
                   <div className="mb-8">
@@ -139,7 +80,7 @@ const Home = ({ products, categories }: IProps) => {
                   </div>
                 )}
               </div>
-            ))}
+            ))} */}
           </section>
         </main>
       </div>
@@ -148,22 +89,4 @@ const Home = ({ products, categories }: IProps) => {
   )
 }
 
-
-
-export const getServerSideProps = async () => {
-  // Fetch data from external API
-  const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
-  const resProducts = await fetch(`${BASE_URL}/products/storefront`);
-
-  const resCategories = await fetch(`${BASE_URL}/categories`);
-
-  const products = await resProducts.json();
-  const categories = await resCategories.json();
-
-  return { props: { products, categories } };
-}
-
-
-
-export default Home;
+export default Ornaments
